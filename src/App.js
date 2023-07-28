0f9df7a936f8714e2a3ae6bae8b4bcb5717c77cc
Akash patil <ap4180530@gmail.com>
2023-07-28 20:01:47 +0530

    edited the data obj

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.css";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    text: "Lodem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
